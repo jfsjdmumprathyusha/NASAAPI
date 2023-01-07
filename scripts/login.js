784116e01a0a4e3023cd0d33020a32eaa84308c1
prathyusha <prathyushaprathyu.ce@gmail.com>
2023-01-07 18:02:25 +0530

    first commit

---
+++ scripts/login.js
@@ -0,0 +1,14 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  function openlogin() {
+    document.getElementById("loginForm").style.display = "block";
+  }
+  
+  function closelogin() {
+    document.getElementById("loginForm").style.display = "none";
+  }
